8bca54f81b145b3413bc7fcf9303a00602e10cec
aakansha <aakansha.rai@miracuves.com>
2024-03-24 12:02:15 +0530

    UI

--- app/src/main/java/com/example/jaavansathiclone/MainActivity.kt
+++ app/src/main/java/com/example/jaavansathiclone/MainActivity.kt
@@ -1,13 +1,23 @@
 package com.example.jaavansathiclone
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var userLoginBtn: LinearLayout
+    private lateinit var adminLoginBtn: LinearLayout
+    private lateinit var registerBtn: CardView
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
@@ -15,6 +25,22 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        userLoginBtn = findViewById(R.id.LinearLoginAsUser)
+        adminLoginBtn = findViewById(R.id.LinearLoginAsAdmin)
+        registerBtn = findViewById(R.id.registerUserCard)
+
+        userLoginBtn.setOnClickListener {
+            startActivity(Intent(this, UserLoginPage::class.java))
+        }
+
+        adminLoginBtn.setOnClickListener {
+            startActivity(Intent(this, AdminLoginPage::class.java))
+        }
+
+        userLoginBtn.setOnClickListener {
+            startActivity(Intent(this, PersonalDetails::class.java))
         }
     }
 }
